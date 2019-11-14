aa4c8d2e1e9855d59a125b1d465d8fec6ada2d7c
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-14 19:39:02 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,18 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
   @@all = []
   
   def initialize(title)
     @title = title
     @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  
+end
 
 
 describe 'Post' do
