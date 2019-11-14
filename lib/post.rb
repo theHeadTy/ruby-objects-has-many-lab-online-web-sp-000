f3b51a374b6be6a3a2486c26a93a28b70e846d1b
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-14 19:41:21 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -14,21 +14,4 @@ class Post
   def author_name
     self.author.nil? nil : self.author.name
   end
-  
-end
-
-
-
-  describe '#author_name' do
-    it 'knows the name of its author' do
-      sophie = Author.new('Sophie')
-      post.author = sophie
-      expect(post.author_name).to eq('Sophie')
-    end
-
-    it 'returns nil if the post does not have an author' do
-      expect(post.author_name).to eq nil
-    end
-  end
-  # binding.pry
 end
